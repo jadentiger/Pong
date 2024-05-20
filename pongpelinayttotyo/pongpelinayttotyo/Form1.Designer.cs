b4fb798a6249c06f30f50c7f00bde8759e0c8381
Jaden Brower <browerjaden7@gmail.com>
2024-05-20 15:20:55 +0300

    Added the game files

---
+++ pongpelinayttotyo/pongpelinayttotyo/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace pongpelinayttotyo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.computer = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Fuchsia;
+            this.player.Location = new System.Drawing.Point(8, 138);
+            this.player.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(17, 87);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // computer
+            // 
+            this.computer.BackColor = System.Drawing.Color.Fuchsia;
+            this.computer.Location = new System.Drawing.Point(577, 138);
+            this.computer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.computer.Name = "computer";
+            this.computer.Size = new System.Drawing.Size(17, 87);
+            this.computer.TabIndex = 1;
+            this.computer.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Fuchsia;
+            this.ball.Location = new System.Drawing.Point(283, 167);
+            this.ball.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(254, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First to 5 loses!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(602, 356);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.computer);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Player: 0 -- Computer: 0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox computer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
